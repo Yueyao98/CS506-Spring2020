aeb7357d96f7cc27ca7f5a5eda9204bd6a9efc1f
Yueyao Zhang <yue2222@bu.edu>
2020-01-29 16:31:44 -0500

    Add existing file

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,8 +1,6 @@
-def draw_lake(length,width):
-    length = 20
-    width = 20
-    for i in range(length):
-    	for j in range(width):
+def draw_lake(length=20,width=20):
+    for _ in range(length):
+    	for _ in range(width):
     		print("~", end = " ")
     	print ()
     return
